64103cef9f4ca7a9d03a5d435067eb39f576acd8
Yuvaraj <yuvaraj.codes@gmail.com>
2025-06-08 09:48:51 +0530

    Time: 0 ms (100%), Space: 45.4 MB (44.26%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int len = nums.length;
+        int sum = (len * (len + 1)) / 2;
+        for(int n : nums){
+            sum -= n;
+        }
+        return sum;
+    }
+}
