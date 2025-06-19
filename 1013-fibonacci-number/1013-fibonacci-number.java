ccab779f400abec6eaf846ab2b8be35576564027
Yuvaraj <yuvaraj.codes@gmail.com>
2025-06-19 08:10:38 +0530

    Time: 0 ms (100%), Space: 40.4 MB (50.9%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,6 +1,15 @@
 class Solution {
     public int fib(int n) {
+        // if(n <= 1) return n;
+        // else return fib(n - 1) + fib(n - 2);
         if(n <= 1) return n;
-        else return fib(n - 1) + fib(n - 2);
+        int a = 0,  b = 1;
+        while(n != 1){
+            int sum = a + b;
+            a = b;
+            b = sum;
+            --n;
+        }
+        return b;
     }
 }
