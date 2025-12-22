398a8a4c0a77cc9617845f0db82e1afabdac55b0
Yuvaraj <yuvaraj.codes@gmail.com>
2025-12-22 05:04:44 -0800

    Time: 0 ms (100%), Space: 49.4 MB (34.49%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,9 +14,13 @@
  * }
  */
 class Solution {
+    private int height(TreeNode root) {
+        return root == null ? -1 : 1 + height(root.left);
+    }
     public int countNodes(TreeNode root) {
-        // o(n) solution
-        if(root == null) return 0;
-        return 1 + countNodes(root.left) + countNodes(root.right);
+        int h = height(root);
+        return h < 0 ? 0 :
+               height(root.right) == h-1 ? (1 << h) + countNodes(root.right)
+                                         : (1 << h-1) + countNodes(root.left);
     }
 }
