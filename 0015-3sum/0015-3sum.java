fd5ea94190685a9e146bcc131f9212e912aa3e48
Yuvaraj <yuvaraj.codes@gmail.com>
2026-01-06 23:41:52 +0530

    Time: 29 ms (90.65%), Space: 59.4 MB (28.39%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,24 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // better solution - uses aux space
-        Set<List<Integer>> tripletSet = new HashSet<>();
+        // No aux space
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
         int n = nums.length;
         for(int i = 0; i < n; ++i){
-            Set<Integer> hashSet = new HashSet<>();
-            for(int j = i + 1; j < n; ++j){
-                int third = -(nums[i] + nums[j]);
-                if(hashSet.contains(third)){
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[j]);
-                    temp.add(third);
-                    Collections.sort(temp);
-                    tripletSet.add(temp);                    
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1, k = n - 1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0) ++j;
+                else if(sum > 0) --k;
+                else{
+                    ans.add(List.of(nums[i], nums[j], nums[k]));
+                    ++j;
+                    --k;
+                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    while(j < k && nums[k] == nums[k + 1]) --k;
                 }
-                hashSet.add(nums[j]);
             }
         }
-        List<List<Integer>> ans = new ArrayList<>(tripletSet);
         return ans;
     }
 }
