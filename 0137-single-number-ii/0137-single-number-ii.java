e642e15178bc3a850db012686172193fef1156ea
Yuvaraj <yuvaraj.codes@gmail.com>
2026-03-16 21:18:27 +0530

    Time: 1 ms (93.35%), Space: 45.2 MB (83.7%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,10 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int n = nums.length;
-        Arrays.sort(nums);
-        for(int i = 1; i < n; i = i + 3){
-            if(nums[i] != nums[i - 1]) return nums[i - 1];
+        int ones = 0, twos = 0;
+        for(int i = 0; i < n; ++i){
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
         }
-        return nums[n - 1];
+        return ones;
     }
 }
