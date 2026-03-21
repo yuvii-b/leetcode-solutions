eb639c147286e0bf0565d1450b3071ae02997977
Yuvaraj <yuvaraj.codes@gmail.com>
2026-03-21 16:44:45 +0530

    Time: 1 ms (85.23%), Space: 45.8 MB (25.53%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,27 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        boolean[] freq = new boolean[nums.length];
-        generate(nums, new ArrayList<>(), freq, result);
+        generate(0, nums, result);
         return result;
     }
 
-    private void generate(int[] nums, List<Integer> curr, boolean[] freq, List<List<Integer>> result){
-        if(curr.size() == nums.length){
-            result.add(new ArrayList<>(curr));
+    private void generate(int index, int[] nums, List<List<Integer>> result){
+        if(index == nums.length){
+            List<Integer> temp = new ArrayList<>();
+            for(int n : nums) temp.add(n);
+            result.add(new ArrayList<>(temp));
             return;
         }
-        for(int i = 0; i < nums.length; ++i){
-            if(!freq[i]){
-                curr.add(nums[i]);
-                freq[i] = true;
-                generate(nums, curr, freq, result);
-                curr.remove(curr.size() - 1);
-                freq[i] = false;
-            }
+        for(int i = index; i < nums.length; ++i){
+            swap(nums, i, index);
+            generate(index + 1, nums, result);
+            swap(nums, i, index);
         }
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
