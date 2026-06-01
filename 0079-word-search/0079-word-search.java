2898adca1f2d90815f942825123102e56c1f7391
Yuvaraj <yuvaraj.codes@gmail.com>
2026-06-01 17:17:30 +0530

    Time: 127 ms (89.69%), Space: 43 MB (58.98%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,37 +1,25 @@
-public class Solution {
-    static boolean[][] visited;
+class Solution {
     public boolean exist(char[][] board, String word) {
-        visited = new boolean[board.length][board[0].length];
-        
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                if((word.charAt(0) == board[i][j]) && search(board, word, i, j, 0)){
-                    return true;
-                }
+        int m = board.length, n = board[0].length;
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(board[i][j] == word.charAt(0) && search(board, word, i, j, 0)) return true;
             }
         }
-        
         return false;
     }
-    
-    private boolean search(char[][]board, String word, int i, int j, int index){
-        if(index == word.length()){
-            return true;
-        }
-        
-        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index) || visited[i][j]){
+
+    private boolean search(char[][] board, String word, int i, int j, int ind){
+        if(ind == word.length()) return true;
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(ind))
             return false;
-        }
-        
-        visited[i][j] = true;
-        if(search(board, word, i-1, j, index+1) || 
-           search(board, word, i+1, j, index+1) ||
-           search(board, word, i, j-1, index+1) || 
-           search(board, word, i, j+1, index+1)){
-            return true;
-        }
-        
-        visited[i][j] = false;
-        return false;
+        char temp = board[i][j];
+        board[i][j] = ' ';
+        boolean ans = search(board, word, i - 1, j, ind + 1) ||
+                      search(board, word, i + 1, j, ind + 1) ||
+                      search(board, word, i, j - 1, ind + 1) || 
+                      search(board, word, i, j + 1, ind + 1);
+        board[i][j] = temp;
+        return ans;
     }
 }
