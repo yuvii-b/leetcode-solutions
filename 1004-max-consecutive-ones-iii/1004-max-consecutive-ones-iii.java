5b033c9f91721ac60c15d636dd4c1e1a7c80ef72
Yuvaraj <yuvaraj.codes@gmail.com>
2026-06-03 18:32:48 +0530

    Time: 2 ms (99.95%), Space: 52.1 MB (53.13%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,16 +1,15 @@
 class Solution {
-    // TC - O(N), SC - O(1) -> change while to if.. note the reason for optimization
+    // TC - O(N), SC - O(1) -> change while to if and remove max, len.. note the reason for optimization
     public int longestOnes(int[] nums, int k) {
-        int n = nums.length, l = 0, r = 0, zeros = 0, len = 0;
+        int n = nums.length, l = 0, r = 0, zeros = 0;
         while(r < n){
             if(nums[r] == 0) ++zeros;
             if(zeros > k){
                 if(nums[l] == 0) --zeros;
                 ++l;
             }
-            len = Math.max(len, r - l + 1);
             ++r;
         }
-        return len;
+        return r - l;
     }
 }
