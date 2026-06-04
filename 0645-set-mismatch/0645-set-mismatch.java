d0ad0d18a01b7a5fb98dab6981a8ab0187cb4477
Yuvaraj <yuvaraj.codes@gmail.com>
2026-06-04 11:11:12 +0530

    Time: 3 ms (65.24%), Space: 47.8 MB (36.85%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,15 +1,19 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int repeating = -1, missing = -1, n = nums.length;
-        int[] count = new int[n + 1];
+        long sum = 0, sumOfN = 0, square = 0, squareOfN = 0, n = nums.length;
         for(int i = 0; i < n; ++i){
-            count[nums[i]]++;
+            sum += nums[i];
+            square += nums[i] * nums[i];
         }
-        for(int i = 1; i <= n; ++i){
-            if(count[i] == 2) repeating = i;
-            else if(count[i] == 0) missing = i;
-            if(repeating != -1 && missing != -1) break;
-        }
-        return new int[]{repeating, missing};
+        sumOfN = n * (n + 1) / 2;
+        squareOfN = n * (n + 1) * (2 * n + 1) / 6;
+        long val1 = sum - sumOfN; // x - y
+        long val2 = square - squareOfN; // x^2 - y^2
+        val2 /= val1;
+        long x = (val1 + val2) / 2;
+        long y = x - val1;
+
+        return new int[]{(int)x, (int)y};
+
     }
 }
