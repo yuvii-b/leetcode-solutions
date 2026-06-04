080a17220363e296ea42f9183d9f0ba4d6d7aec1
Yuvaraj <yuvaraj.codes@gmail.com>
2026-06-04 11:00:15 +0530

    Time: 2 ms (94.98%), Space: 47.8 MB (25.64%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int repeating = -1, missing = -1, n = nums.length;
+        int[] count = new int[n + 1];
+        for(int i = 0; i < n; ++i){
+            count[nums[i]]++;
+        }
         for(int i = 1; i <= n; ++i){
-            int count = 0;
-            for(int j = 0; j < n; ++j){
-                if(nums[j] == i) ++count;
-            }
-            if(count == 2) repeating = i;
-            else if(count == 0) missing = i;
+            if(count[i] == 2) repeating = i;
+            else if(count[i] == 0) missing = i;
             if(repeating != -1 && missing != -1) break;
         }
         return new int[]{repeating, missing};
