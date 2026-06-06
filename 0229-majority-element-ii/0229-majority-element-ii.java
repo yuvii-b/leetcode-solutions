139dd7a6f709965594f94bdd1e90f932c4a95666
Yuvaraj <yuvaraj.codes@gmail.com>
2026-06-06 10:10:29 +0530

    Time: 2 ms (99.92%), Space: 52.4 MB (63.87%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,16 +1,18 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> res = new ArrayList<>();
-        int cnt1 = 0, cnt2 = 0, ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE, n = nums.length;
+        int n = nums.length, cnt1 = 0, cnt2 = 0, ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE;
+        List<Integer> ans = new ArrayList<>();
         for(int i = 0; i < n; ++i){
-            if(cnt1 == 0 && nums[i] != ele2){
-                cnt1 = 1;
+            if(ele1 == nums[i]) ++cnt1;
+            else if(ele2 == nums[i]) ++cnt2;
+            else if(cnt1 == 0){
                 ele1 = nums[i];
-            }else if(cnt2 == 0 && nums[i] != ele1){
-                cnt2 = 1;
+                cnt1 = 1;
+            }
+            else if(cnt2 == 0){
                 ele2 = nums[i];
-            }else if(nums[i] == ele1) ++cnt1;
-            else if(nums[i] == ele2) ++cnt2;
+                cnt2 = 1;
+            }
             else{
                 --cnt1;
                 --cnt2;
@@ -19,11 +21,11 @@ class Solution {
         cnt1 = 0; cnt2 = 0;
         for(int i = 0; i < n; ++i){
             if(nums[i] == ele1) ++cnt1;
-            if(nums[i] == ele2) ++cnt2;
+            else if(nums[i] == ele2) ++cnt2;
         }
-        int expected = n / 3 + 1;
-        if(cnt1 >= expected) res.add(ele1);
-        if(cnt2 >= expected && ele1 != ele2) res.add(ele2);
-        return res;
+        int expected = n / 3;
+        if(cnt1 > expected) ans.add(ele1);
+        if(cnt2 > expected) ans.add(ele2);
+        return ans;
     }
 }
