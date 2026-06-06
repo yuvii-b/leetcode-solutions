46fdecf281c98090b62474b5ee6599ed0ab92a17
Yuvaraj <yuvaraj.codes@gmail.com>
2026-06-06 13:43:20 +0530

    Time: 1 ms (92.13%), Space: 47.1 MB (86.05%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,11 +3,11 @@ class Solution {
         int prefix = 1, suffix = 1, n = nums.length;
         int ans = Integer.MIN_VALUE; 
         for(int i = 0; i < n; ++i){
-            if(prefix == 0) prefix = 1;
-            if(suffix == 0) suffix = 1;
             prefix *= nums[i];
             suffix *= nums[n - i - 1];
             ans = Math.max(ans, Math.max(prefix, suffix));
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
         }
         return ans;
     }
