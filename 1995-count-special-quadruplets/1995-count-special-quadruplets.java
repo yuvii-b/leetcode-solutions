5d529bea723cfddb2a2582dc6028c1d46c67f268
Yuvaraj <yuvaraj.codes@gmail.com>
2026-06-07 14:05:38 +0530

    Time: 12 ms (85.6%), Space: 45 MB (14.65%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.java
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.java
@@ -1,13 +1,17 @@
 class Solution {
+    // nums[a] + nums[b] == nums[d] - nums[c]
     public int countQuadruplets(int[] nums) {
         int count = 0, n = nums.length;
-        for(int i = 0; i < n; ++i){
-            for(int j = i + 1; j < n; ++j){
-                for(int k = j + 1; k < n; ++k){
-                    for(int l = k + 1; l < n; ++l){
-                        if(nums[i] + nums[j] + nums[k] == nums[l]) ++count;
-                    }
-                }
+        Map<Integer, Integer> seen = new HashMap<>();
+        for(int b = n - 3; b >= 1; --b){
+            int c = b + 1;
+            for(int d = c + 1; d < n; ++d){
+                int diff = nums[d] - nums[c];
+                seen.put(diff, seen.getOrDefault(diff, 0) + 1);
+            }
+            for(int a = b - 1; a >= 0; --a){
+                int sum = nums[a] + nums[b];
+                count += seen.getOrDefault(sum, 0);
             }
         }
         return count;
