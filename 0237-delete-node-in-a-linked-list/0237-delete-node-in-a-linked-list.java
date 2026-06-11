1772b9946a2b25a8ef8a4f8bed1eaafb16e95eeb
Yuvaraj <yuvaraj.codes@gmail.com>
2026-06-11 16:57:47 +0530

    Time: 0 ms (100%), Space: 45.1 MB (61.27%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -1,11 +1,9 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+/*
+How to become another person in the world? Two steps.
+One, change your appearance to whom you want to be.
+Two, kill that person 
+*/
+
 class Solution {
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
