63f9c7fa5b83b135327ff4a0b4abe08817e04e19
Yuvaraj <yuvaraj.codes@gmail.com>
2026-06-23 17:55:59 +0530

    Time: 4 ms (74.43%), Space: 45.5 MB (22.17%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nge = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+        int n = nums2.length;
+        for(int i = n - 1; i >= 0; --i){
+            while(!st.isEmpty() && st.peek() <= nums2[i]) st.pop();
+            nge.put(nums2[i], (st.isEmpty() ? -1 : st.peek()));
+            st.push(nums2[i]);
+        }
+        n = nums1.length;
+        int[] ans = new int[n];
+        for(int i = 0; i < n; ++i){
+            ans[i] = nge.get(nums1[i]);
+        }
+        return ans;
+    }
+}
