6611f81d6afbc99f56fcc6d7b217ff69c77f32dd
Yuvaraj <yuvaraj.codes@gmail.com>
2026-06-29 16:59:11 +0530

    Time: 2 ms (97.54%), Space: 44.4 MB (95.94%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,14 +1,26 @@
 class Solution {
+    // 2 pointer solution
     public int longestValidParentheses(String s) {
-        Deque<Integer> st = new ArrayDeque<>();
-        int maxLen = 0;
-        st.push(-1);
-        for(int i = 0; i < s.length(); ++i){
-            if(s.charAt(i) == '(') st.push(i);
-            else{
-                st.pop();
-                if(st.isEmpty()) st.push(i);
-                else maxLen = Math.max(maxLen, i - st.peek());
+        int n = s.length(), left = 0, right = 0, maxLen = 0;
+        for(int i = 0; i < n; ++i){
+            char ch = s.charAt(i);
+            if(ch == '(') ++left;
+            else ++right;
+            if(left == right) maxLen = Math.max(maxLen, 2 * left);
+            else if(right > left) {
+                left = 0;
+                right = 0;
+            }
+        }
+        left = 0; right = 0;
+        for(int i = n - 1; i >= 0; --i){
+            char ch = s.charAt(i);
+            if(ch == '(') ++left;
+            else ++right;
+            if(left == right) maxLen = Math.max(maxLen, 2 * right);
+            else if(left > right){
+                left = 0;
+                right = 0;
             }
         }
         return maxLen;
