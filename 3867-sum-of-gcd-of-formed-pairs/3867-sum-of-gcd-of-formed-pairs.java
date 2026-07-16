2fcc685e51a3230f6ad644f3b5018e85255803af
Yuvaraj <yuvaraj.codes@gmail.com>
2026-07-16 22:24:13 +0530

    Time: 57 ms (49.5%), Space: 107.7 MB (96.5%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
@@ -15,7 +15,11 @@ class Solution {
     }
 
     private int gcd(int a, int b){
-        if(b == 0) return a;
-        return gcd(b, a % b);
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
