e2dfd56b1adf492c7b5dd49f5342f242de68eb68
Yuvaraj <yuvaraj.codes@gmail.com>
2026-07-25 19:42:40 +0530

    Time: 2189 ms (5.18%), Space: 77.4 MB (5.03%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,10 +1,37 @@
 class Solution {
+    // use min heap preferrably, this is quick select, for even more efficiency, randomize the pivot
     public int findKthLargest(int[] nums, int k) {
-        Queue<Integer> pq = new PriorityQueue<>();
-        for(int num : nums){
-            pq.offer(num);
-            if(pq.size() > k) pq.poll();
+        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+     private int quickSelect(int[] nums, int low, int high, int k) {
+        while (low <= high) {
+            int pivot = partition(nums, low, high);
+            if (pivot == k) return nums[pivot];
+            else if (pivot > k) high = pivot - 1;
+            else low = pivot + 1;
         }
-        return pq.peek();
+        return -1;
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int i = low, j = high;
+        Random rand = new Random();
+        int random = low + rand.nextInt(high - low + 1);
+        swap(arr, random, low);
+        int pivot = arr[low];
+        while (i < j) {
+            while (arr[i] <= pivot && i < high) i++;
+            while (arr[j] > pivot && j > low) j--;
+            if (i < j) swap(arr, i, j);
+        }
+        swap(arr, low, j);
+        return j;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
