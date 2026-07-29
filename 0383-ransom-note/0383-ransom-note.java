23978c9864a13b6fc751e027ef84e89a78b94ee5
Yuvaraj <yuvaraj.codes@gmail.com>
2026-07-29 23:08:37 +0530

    Time: 1 ms (99.98%), Space: 46.5 MB (57.15%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length() > magazine.length()) return false;
+        int[] freq = new int[26];
+        for(char c : magazine.toCharArray()) freq[c - 'a']++;
+        for(char c : ransomNote.toCharArray()){
+            if(freq[c - 'a'] == 0) return false;
+            freq[c - 'a']--;
+        }
+        return true;
+    }
+}
